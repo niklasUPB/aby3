ea5cb1cb01627dfa71d85f7a5f7445de8d3296a4
niklas <105489239+niklasUPB@users.noreply.github.com>
2022-10-09 14:51:41 +0200

    Update use_case2_fast.sh

--- use_case2_fast.sh
+++ use_case2_fast.sh
@@ -1,9 +1,9 @@
 var=1
-while [ -e "./Results_2/use_case2_fast_local_$1_$var" ]
+while [ -e "./Results_2_fast/use_case2_fast_local_$1_$var" ]
 do
 	echo test123
 	var=$(($var + 1))
 done
 #python3 build.py
 cd bin
-time --output=../Results_2/use_case2_fast_local_$1_$var  -a -v ./use_case2_fast $1 $2 >> ../Results_2_fast/use_case1_fast_local_$1_$var
+time --output=../Results_2_fast/use_case2_fast_local_$1_$var  -a -v ./use_case2_fast $1 $2 >> ../Results_2_fast/use_case1_fast_local_$1_$var
